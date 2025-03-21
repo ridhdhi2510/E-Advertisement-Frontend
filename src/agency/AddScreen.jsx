843a629c5f9395a3ff5fbed3b8b08ffd79d4e482
Anikets3103 <tekina3103@gmail.com>
2025-03-21 12:40:27 +0530

    added AddScreen css file

--- src/agency/AddScreen.jsx
+++ src/agency/AddScreen.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import "../assets/css/addScreen.css"
 
 export const AddScreen = () => {
   const [states, setStates] = useState([]);
@@ -61,7 +62,7 @@ export const AddScreen = () => {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gradient-to-b from-blue-100 to-blue-50 p-6">
-    <div className="w-full max-w-2xl bg-white dark:bg-gray-900 p-8 rounded-3xl shadow-lg transition-all duration-300">
+    <div className="w-full max-w-2xl bg-white dark:bg-gray-900 p-8 rounded-3xl shadow-lg transition-all duration-300 container">
       {/* Form Title */}
       <h2 className="text-center text-3xl font-bold text-gray-800 dark:text-white mb-6">
         Add New Hoarding
