1286fbe664501fb7a4dd05d1abc04b9cef49058b
Anikets3103 <tekina3103@gmail.com>
2025-03-26 09:40:37 +0530

    added dashboard option in both sidebars and updated bookinghoarding with states, cities, areas

--- src/layouts/AgencyPage.jsx
+++ src/layouts/AgencyPage.jsx
@@ -29,6 +29,13 @@ import {
   SpaceDashboardRounded as SpaceDashboardRoundedIcon,
 } from "@mui/icons-material";
 import { Outlet, useNavigate } from "react-router-dom";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import HomeIcon from "@mui/icons-material/Home";
+import TvIcon from "@mui/icons-material/Tv";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+
+
 
 const drawerWidth = 240;
 
@@ -233,11 +240,20 @@ export default function AgencyPage() {
                   },
                 ]}
               >
-                <SpaceDashboardRoundedIcon />
+                <HomeIcon /> 
               </ListItemIcon>
               <ListItemText primary="Home" sx={{ opacity: open ? 1 : 0 }} />
             </ListItemButton>
           </ListItem>
+
+            {/* for agency dashboard navigation */}
+          <ListItem disablePadding onClick={() => navigate("/agency")}>
+            <ListItemButton>
+              <ListItemIcon><DashboardIcon /></ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItemButton>
+          </ListItem>
+
 
           {/*  add Screen page  Navigation */}
           <ListItem
@@ -300,7 +316,7 @@ export default function AgencyPage() {
                   },
                 ]}
               >
-                <AddIcCallSharpIcon />
+                <TvIcon />
               </ListItemIcon>
               <ListItemText
                 primary="View My Screens"
@@ -313,7 +329,7 @@ export default function AgencyPage() {
 
         <List>
           {/* for Home navigation */}
-          <ListItem
+          {/* <ListItem
             disablePadding
             sx={{ display: "block" }}
             onClick={() => {
@@ -346,10 +362,10 @@ export default function AgencyPage() {
                 sx={{ opacity: open ? 1 : 0 }}
               />
             </ListItemButton>
-          </ListItem>
+          </ListItem> */}
 
           {/*  About page  Navigation */}
-          <ListItem
+          {/* <ListItem
             disablePadding
             sx={{ display: "block" }}
             onClick={() => {
@@ -378,7 +394,7 @@ export default function AgencyPage() {
               </ListItemIcon>
               <ListItemText primary="About" sx={{ opacity: open ? 1 : 0 }} />
             </ListItemButton>
-          </ListItem>
+          </ListItem> */}
 
           {/* contact page navigation */}
 
@@ -410,7 +426,7 @@ export default function AgencyPage() {
 
                 }}
               >
-                <AddIcCallSharpIcon />
+                <LogoutIcon />
               </ListItemIcon>
               <ListItemText primary="Logout" sx={{ opacity: open ? 1 : 0 }} />
             </ListItemButton>
